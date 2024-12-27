c66a0cf8efe424468587c1f584bab09f5000c448
Simon Bogutzky <s.bogutzky@reply.de>
2024-12-27 10:51:22 +0100

    Adds dependencies

--- Package.swift
+++ Package.swift
@@ -1,24 +1,32 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "FinancialEntities",
+    platforms: [
+            .iOS(.v13)
+        ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FinancialEntities",
             targets: ["FinancialEntities"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.0"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FinancialEntities"),
+            name: "FinancialEntities",
+            swiftSettings: [
+                .unsafeFlags(["-strict-concurrency=complete"])
+            ],
+        plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]),
         .testTarget(
             name: "FinancialEntitiesTests",
-            dependencies: ["FinancialEntities"]
+            dependencies: ["FinancialEntities"],
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")]
         ),
     ]
 )
